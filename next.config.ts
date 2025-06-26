41b7a1e8b9b43de43e41a81d6d51cd6c375f4e27
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-06-26 12:04:38 +0700

    Remove unused remote image patterns from Next.js configuration to streamline image handling.

--- next.config.ts
+++ next.config.ts
@@ -10,14 +10,6 @@ const nextConfig: NextConfig = withBundleAnalyzer({
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-      },
-      {
-        protocol: "https",
         hostname: "res.cloudinary.com",
       },
     ],
